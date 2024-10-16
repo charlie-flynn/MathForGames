2ee79e15a81f319d00d4eeac79c28469eaaf1761
charlie-flynn <charlie.flynn.business@gmail.com>
2024-10-16 12:16:18 -0500

    moved the meem outta the way (make sure to exclude it from the project) and added DotProduct to Vector2

--- MathLibrary/Vector2.cs
+++ MathLibrary/Vector2.cs
@@ -7,20 +7,57 @@ using System.Numerics;
 
 namespace MathLibrary
 {
-    public class Vector2
+    public struct Vector2
     {
-        float x, y;
+        public float x, y;
 
-        public Vector2()
+        public float Magnitude
         {
-            x = 0;
-            y = 0;
+            get
+            {
+                return (float)Math.Abs(Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)));
+            }
+        }
+        public Vector2 Normalized
+        {
+            get
+            {
+                return this / Magnitude;
+            }
         }
 
-        public Vector2(float x, float y)
+        public Vector2(float x = 0, float y = 0)
         {
             this.x = x;
             this.y = y;
+        }
+
+        public Vector2 Normalize()
+        {
+            this = Normalized;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return "(" + x + ", " + y + ")";  
+        }
+
+        public float DotProduct(Vector2 other)
+        {
+            return (x * other.x) + (y * other.y);
+        }
+
+        // operator override for equal sign
+        public static bool operator ==(Vector2 left, Vector2 right)
+        {
+            return (left.x == right.x) && (left.y == right.y);
+        }
+
+        // legally required override for not equal sign
+        public static bool operator !=(Vector2 left, Vector2 right)
+        {
+            return !(left == right);
         }
 
         // operator override for addition
@@ -35,33 +72,28 @@ namespace MathLibrary
             return new Vector2(left.x - right.x, left.y - right.y);
         }
 
-        // operator override for multiplication by another vector
-        public static Vector2 operator *(Vector2 left, Vector2 right)
-        {
-            return new Vector2(left.x * right.x, left.y - right.y);
-        }
-
         // operator override for multiplication by a scalar
-        public static Vector2 operator *(Vector2 left, float right)
+        public static Vector2 operator *(Vector2 left, float scalar)
         {
-            return new Vector2(left.x * right, left.y * right);
+            return new Vector2(left.x * scalar, left.y * scalar);
         }
 
         // operator override for division in a polite manner (will not divide by zero)
-        public static Vector2 operator /(Vector2 left, Vector2 right)
+        public static Vector2 operator /(Vector2 left, float scalar)
         {
             float resultX = 0;
             float resultY = 0;
-
-            if (left.x != 0 && right.x != 0)
+            if (scalar != 0)
             {
-                resultX = left.x / right.x;
+                if (left.x != 0 && scalar != 0)
+                {
+                    resultX = left.x / scalar;
+                }
+                if (left.y != 0 && scalar != 0)
+                {
+                    resultY = left.y / scalar;
+                }
             }
-            if (left.y != 0 && right.y != 0)
-            {
-                resultY = left.y / right.y;
-            }
-
             return new Vector2(resultX, resultY);
         }
 
@@ -71,7 +103,7 @@ namespace MathLibrary
             return new Vector2(vector.X, vector.Y);
         }
 
-        // that but the other way around
+        // ^^^ that but the other way around
         public static implicit operator System.Numerics.Vector2(Vector2 vector)
         {
             return new System.Numerics.Vector2(vector.x, vector.y);
